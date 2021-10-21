ef58d27bd8931d1fec0f9b17e9dc73ff55b8dc5c
Hungry Shark <airuuuww@gmail.com>
2021-10-21 12:22:47 +0300

    HW3

--- app/src/main/java/com/hungryshark/lesson1/utils/screens/IScreens.kt
+++
@@ -1,9 +0,0 @@
-package com.hungryshark.lesson1.utils.screens
-
-import com.github.terrakok.cicerone.Screen
-import com.hungryshark.lesson1.data.model.GithubUser
-
-interface IScreens {
-    fun openUserListFragment(): Screen
-    fun openUserDetailsFragment(user: GithubUser): Screen
-}
